18229623e81aa456e9c70dc4de405e04b42f34e7
Rafael-Mourato <84647233+Rafael-Mourato@users.noreply.github.com>
2023-05-20 14:36:50 +0100

    New Features
    UI - Services
    Add Galery
    Other corrections

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+let count = 1
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+  nextImage();
+}, 2500)
+
+function nextImage(){
+  count++;
+  if(count>14){
+    count = 1;
+  }
+  document.getElementById("radio"+count).checked = true;
+}
+
 $(document).ready(function() {
   $('a').on('click', function(e) {
     if (this.hash !== '') {
